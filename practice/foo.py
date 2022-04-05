65cad79ee4b41ebd553588821e3507c18e9afa32
dengmingcong <mc.deng@outlook.com>
2022-04-05 22:27:45 +0800

    feat: test two feats

--- practice/foo.py
+++ practice/foo.py
@@ -8,3 +8,11 @@ def add_code_to_test_prefix_refactor():
 
 def add_code_to_test_prefix_fix():
     pass
+
+
+def add_code_to_test_prefix_feat():
+    pass
+
+
+def add_code_to_test_prefix_feat_two():
+    pass
